9fb46f34d72e31c6b1bf5375b197a98119bf524a
Pawel Saczawa <pawel.saczawa@scalac.io>
2017-03-31 22:28:43 +0200

    Introduce PieceType
    Queen as first possible PieceType

--- prog.hs
+++ prog.hs
@@ -2,14 +2,16 @@ import Test.HUnit
 import qualified Data.Set as Set
 import qualified Data.List as List
 
-test1 = TestCase (assertChessSolution "0x0 should give 1 empty solution" [[]] (solve 0 0))
-test2 = TestCase (assertChessSolution "1x1 should give 1 solutions" [[(1, 1)]] (solve 1 1))
+test1 = TestCase (assertChessSolution "0x0 should give 1 empty solution" [[]] (solve 0 0 []))
+test2 = TestCase (assertChessSolution "1x1 should give 1 solutions" [[(Queen, (1, 1))]] (solve 1 1 [(Queen, 1)]))
 
-test3 = TestCase (assertChessSolution "4x4 with 2 Queens should give 2 solutions" [[(1, 2), (2, 4), (3, 1), (4, 3)]
-                                                                          ,[(1, 3), (2, 1), (3, 4), (4, 2)]
+test3 = TestCase (assertChessSolution "4x4 with 2 Queens should give 2 solutions" [
+                                                                           [(Queen, (1, 2)), (Queen, (2, 4)), (Queen, (3, 1)), (Queen, (4, 3))]
+                                                                          ,[(Queen, (1, 3)), (Queen, (2, 1)), (Queen, (3, 4)), (Queen, (4, 2))]
                                                                           ]
-                                                                          (solve 4 4))
+                                                                          (solve 4 4 [(Queen, 4)]))
 
+data PieceType = Queen deriving (Eq, Ord, Show)
 
 tests = TestList [TestLabel "test1" test1
                  ,TestLabel "test2" test2
@@ -21,19 +23,24 @@ main = runTestTT tests
 genSpace :: Int -> Int -> [(Int, Int)]
 genSpace x y = [(a, b) | a <- [1..x], b <- [1..y]]
 
-type Solution = [(Int, Int)]
+type Solution = [(PieceType, (Int, Int))]
 
-collide (x, y) fieldList = any (\(f1, f2) -> (f1 == x) || (f2 == y) || abs(f1 - x) == abs(f2 - y)) fieldList
+collide (x, y) fieldList = any (\(Queen, (f1, f2)) -> (f1 == x) || (f2 == y) || abs(f1 - x) == abs(f2 - y)) fieldList
 
 
-sol _ 0 found = [found]
+sol :: [(Int, Int)] -> [PieceType]-> Solution -> [Solution]
+sol _ [] found = [found]
 sol [] _ found = []
-sol (firstField : spaceLeft) piecesLeft found
-        | collide firstField found = sol spaceLeft piecesLeft found
-        | otherwise = sol spaceLeft (piecesLeft - 1) (firstField : found) ++ sol spaceLeft piecesLeft found
+sol (firstField : spaceLeft) allPieces@(nextPiece : piecesLeft) found
+        | collide firstField found = sol spaceLeft allPieces found
+        | otherwise = sol spaceLeft piecesLeft ((nextPiece, firstField) : found) ++ sol spaceLeft allPieces found
 
-solve :: Int -> Int -> [Solution]
-solve x y = sol (genSpace x y) x []
+solve :: Int -> Int -> [(PieceType, Int)] -> [Solution]
+solve x y pieces = sol (genSpace x y) (producePieces pieces) []
 
 assertChessSolution label expected actual = assertEqual label (toSet expected) (toSet actual)
         where toSet = Set.fromList . List.map Set.fromList
+
+producePieces :: [(PieceType, Int)] -> [PieceType]
+producePieces [] = []
+producePieces ((pieceType, amount) : rest) = replicate amount pieceType
