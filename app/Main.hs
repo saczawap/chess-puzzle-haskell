64e835aa91ae30a649c43a3eb15c4e57df689713
Paweł Sączawa <saczawap@gmail.com>
2017-05-12 22:24:03 +0200

    Minor clean up

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 
-import Lib
 import PieceType
 import ChessPuzzle
 
 main :: IO ()
-main = print $ solve 7 7 [(King, 2), (Queen, 2), (Bishop, 2), (Knight, 1)]
+main = print . length $
+  solve 7 7 [(King, 2), (Queen, 2), (Bishop, 2), (Knight, 1)]
--- src/PieceType.hs
+++ src/PieceType.hs
@@ -6,13 +6,13 @@ type Point = (Int, Int)
 data PieceType = Queen | Rook | King | Bishop | Knight deriving (Eq, Ord, Show)
 
 pieceCollide :: (PieceType, (Int, Int)) -> (Int, Int) -> Bool
-pieceCollide (Queen, p1) p2 = (onStraightLine p1 p2) || (onDiagonal p1 p2)
+pieceCollide (Queen, p1) p2 = onStraightLine p1 p2 || onDiagonal p1 p2
 pieceCollide (Rook, p1) p2 = onStraightLine p1 p2
-pieceCollide (King, (x1, y1)) (x2, y2) = 
+pieceCollide (King, (x1, y1)) (x2, y2) =
     (abs (x1 - x2) <= 1) && (abs (y1 - y2) <= 1)
 pieceCollide (Bishop, p1) p2 = onDiagonal p1 p2
-pieceCollide (Knight, (x1, y1)) (x2, y2) = 
-    ((abs (x1 - x2) == 1) && (abs (y1 - y2) == 2)) || 
+pieceCollide (Knight, (x1, y1)) (x2, y2) =
+    ((abs (x1 - x2) == 1) && (abs (y1 - y2) == 2)) ||
         ((abs (x1 - x2) == 2) && (abs (y1 - y2) == 1))
 
 onStraightLine :: Point -> Point -> Bool
