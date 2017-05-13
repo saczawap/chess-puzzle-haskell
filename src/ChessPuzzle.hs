dca0e704f0ae306ad96327e2539ad78a93cc27a4
Paweł Sączawa <saczawap@gmail.com>
2017-05-13 12:19:09 +0200

    Fix indentation

--- src/ChessPuzzle.hs
+++ src/ChessPuzzle.hs
@@ -11,19 +11,19 @@ type SolutionSpace = [Point]
 
 solve :: Int -> Int -> [(PieceType, Int)] -> [Solution]
 solve x y pieces = List.concatMap (\pieces -> solve' possibleSpace pieces []) possiblePieces
-    where possibleSpace = genSpace x y
-          possiblePieces = producePieces pieces
+  where possibleSpace = genSpace x y
+        possiblePieces = producePieces pieces
 
 solve' :: SolutionSpace -> [PieceType]-> Solution -> [Solution]
 solve' _ [] found = [found]
 solve' [] _ _found = []
 solve' (firstField : spaceLeft) allPieces@(nextPiece : piecesLeft) found
-        | collide (nextPiece, firstField) found = otherSolutions
-        | otherwise = solve' trimmedSpace piecesLeft pieceAdded ++ otherSolutions
-        where otherSolutions = solve' spaceLeft allPieces found
-              pieceToPlace = (nextPiece, firstField)
-              pieceAdded = pieceToPlace : found
-              trimmedSpace = trimSpace pieceToPlace spaceLeft
+  | collide (nextPiece, firstField) found = otherSolutions
+  | otherwise = solve' trimmedSpace piecesLeft pieceAdded ++ otherSolutions
+  where otherSolutions = solve' spaceLeft allPieces found
+        pieceToPlace = (nextPiece, firstField)
+        pieceAdded = pieceToPlace : found
+        trimmedSpace = trimSpace pieceToPlace spaceLeft
 
 genSpace :: Int -> Int -> SolutionSpace
 genSpace x y = [(a, b) | a <- [1..x], b <- [1..y]]
@@ -37,4 +37,5 @@ trimSpace alreadyPlaced = filter (not . pieceCollide alreadyPlaced)
 producePieces :: [(PieceType, Int)] -> [[PieceType]]
 producePieces [] = [[]]
 producePieces toPlace =
-    List.nub $ List.permutations $ concatMap (\(x, y) -> replicate y x) toPlace
+  List.nub $ List.permutations $ concatMap (\(x, y) -> replicate y x) toPlace
+  
--- src/PieceType.hs
+++ src/PieceType.hs
@@ -9,11 +9,11 @@ pieceCollide :: (PieceType, (Int, Int)) -> (Int, Int) -> Bool
 pieceCollide (Queen, p1) p2 = onStraightLine p1 p2 || onDiagonal p1 p2
 pieceCollide (Rook, p1) p2 = onStraightLine p1 p2
 pieceCollide (King, (x1, y1)) (x2, y2) =
-    (abs (x1 - x2) <= 1) && (abs (y1 - y2) <= 1)
+  (abs (x1 - x2) <= 1) && (abs (y1 - y2) <= 1)
 pieceCollide (Bishop, p1) p2 = onDiagonal p1 p2
 pieceCollide (Knight, (x1, y1)) (x2, y2) =
-    ((abs (x1 - x2) == 1) && (abs (y1 - y2) == 2)) ||
-        ((abs (x1 - x2) == 2) && (abs (y1 - y2) == 1))
+  ((abs (x1 - x2) == 1) && (abs (y1 - y2) == 2)) ||
+  ((abs (x1 - x2) == 2) && (abs (y1 - y2) == 1))
 
 onStraightLine :: Point -> Point -> Bool
 onStraightLine (x1, y1) (x2, y2) = (x1 == x2) || (y1 == y2)
