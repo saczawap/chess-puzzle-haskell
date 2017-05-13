335f3c05a76d11de1e2d76315c7f136d74e1087f
Paweł Sączawa <saczawap@gmail.com>
2017-05-13 11:58:22 +0200

    Some cleanup

--- src/ChessPuzzle.hs
+++ src/ChessPuzzle.hs
@@ -5,29 +5,36 @@ import PieceType
 import qualified Data.Set as Set
 import qualified Data.List as List
 
-genSpace :: Int -> Int -> [(Int, Int)]
-genSpace x y = [(a, b) | a <- [1..x], b <- [1..y]]
-
-type Solution = [(PieceType, (Int, Int))]
-
-collide :: (PieceType, (Int, Int)) -> [(PieceType, (Int, Int))] -> Bool
-collide piece@(newPt, newCoord) = any (\(_pt, field) -> pieceCollide piece field)
-
-
-sol :: [(Int, Int)] -> [PieceType]-> Solution -> [Solution]
-sol _ [] found = [found]
-sol [] _ _found = []
-sol (firstField : spaceLeft) allPieces@(nextPiece : piecesLeft) found
-        | collide (nextPiece, firstField) found = otherSolutions
-        | otherwise = sol (trimSpace spaceLeft (nextPiece, firstField)) piecesLeft ((nextPiece, firstField) : found) ++ otherSolutions
-        where otherSolutions = sol spaceLeft allPieces found
-
-trimSpace :: [Point] -> (PieceType, Point)  -> [Point]
-trimSpace a b = filter (not . pieceCollide b) a
+type PlacedPiece = (PieceType, Point)
+type Solution = [PlacedPiece]
+type SolutionSpace = [Point]
 
 solve :: Int -> Int -> [(PieceType, Int)] -> [Solution]
-solve x y pieces = List.concatMap (\z -> sol (genSpace x y) z []) (producePieces pieces)
+solve x y pieces = List.concatMap (\pieces -> solve' possibleSpace pieces []) possiblePieces
+    where possibleSpace = genSpace x y
+          possiblePieces = producePieces pieces
+
+solve' :: SolutionSpace -> [PieceType]-> Solution -> [Solution]
+solve' _ [] found = [found]
+solve' [] _ _found = []
+solve' (firstField : spaceLeft) allPieces@(nextPiece : piecesLeft) found
+        | collide (nextPiece, firstField) found = otherSolutions
+        | otherwise = solve' trimmedSpace piecesLeft pieceAdded ++ otherSolutions
+        where otherSolutions = solve' spaceLeft allPieces found
+              pieceToPlace = (nextPiece, firstField)
+              pieceAdded = pieceToPlace : found
+              trimmedSpace = trimSpace pieceToPlace spaceLeft
+
+genSpace :: Int -> Int -> SolutionSpace
+genSpace x y = [(a, b) | a <- [1..x], b <- [1..y]]
+
+collide :: PlacedPiece -> Solution -> Bool
+collide piece = any (\(_pt, field) -> pieceCollide piece field)
+
+trimSpace :: PlacedPiece -> SolutionSpace -> SolutionSpace
+trimSpace alreadyPlaced = filter (not . pieceCollide alreadyPlaced)
 
 producePieces :: [(PieceType, Int)] -> [[PieceType]]
 producePieces [] = [[]]
-producePieces toPlace = List.nub $ List.permutations $ concatMap (\(x, y) -> replicate y x) toPlace
+producePieces toPlace =
+    List.nub $ List.permutations $ concatMap (\(x, y) -> replicate y x) toPlace
